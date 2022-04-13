39f07b57a0f119d790465239bb1670ffe92268ee
RomanOmelianenko <r_omelyanenko@ukr.net>
2022-04-13 20:08:27 +0300

    new fix

--- js/main.js
+++ js/main.js
@@ -1,32 +1,46 @@
+// burgerMenu
 const burgerMenu = () => {
   const burger = document.querySelector('.menu-burger img');
   const menuClose = document.querySelector('.header__menu-close');
   const menuMobile = document.querySelector('.header');
 
-  burger.addEventListener('click', () => {
-    menuMobile.style.display = 'block';
-  });
+  burger.addEventListener('click', openBurger);
+  menuClose.addEventListener('click', closeMenuBurger);
 
-  menuClose.addEventListener('click', () => {
+  function openBurger() {
+    menuMobile.style.display = 'block';
+  };
+
+  function closeMenuBurger() {
     menuMobile.style.display = 'none';
-  })
+  };
 };
 
+// modalTrailer
 const modalTrailer = () => {
   const modalOpen = document.querySelector('.modal');
   const buttonPlay = document.querySelector('.main__button-play');
+  const modalIframeSrc = document.querySelector('.modal__inner iframe');
 
-  buttonPlay.addEventListener('click', () => {
+  buttonPlay.addEventListener('click', openModalTrailer);
+  modalOpen.addEventListener('click', closeModalTrailer);
+
+  function openModalTrailer() {
+    modalIframeSrc.src = 'https://www.youtube.com/embed/zC6-H-EcEmU';
     modalOpen.classList.add('active');
-  })
+    window.addEventListener('keydown', closeModalTrailer);
+  };
 
-  modalOpen.addEventListener('click', (e) => {
-    const isModal = e.target.closest('.modal__inner');
+  function closeModalTrailer(e) {
+    const isModalTrailer = e.target.closest('.modal__inner');
 
-    if (!isModal) {
+    if (!isModalTrailer || e.code === "Escape") {
     modalOpen.classList.remove('active');
+    modalIframeSrc.src = ' ';
+    console.log('click');
+    window.removeEventListener('keydown', closeModalTrailer);
     };
-  });
+  };
 };
 
 burgerMenu();
